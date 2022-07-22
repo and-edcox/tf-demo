0fb9f13f136b0ecda8bd621d554feec25a1904b3
and-edcox <edward.cox@and.digital>
2022-07-22 13:49:17 +0100

    Add security group

--- prod.tf
+++ prod.tf
@@ -13,3 +13,33 @@ resource "aws_s3_bucket_acl" "prod_tf_course_acl" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["163.116.162.117/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["163.116.162.117/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["163.116.162.117/32"]
+  }
+
+  tags = {
+    Terraform = "true"
+  }
+}
