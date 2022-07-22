2b865953af41f8cedd2cfa43de71e367bc225c82
and-edcox <edward.cox@and.digital>
2022-07-22 16:32:07 +0100

    added lb

--- prod.tf
+++ prod.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   profile = "default"
-  region  = "eu-west-2"
+  region  = "eu-west-1"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "prod-tf-course-edc"
+  bucket = "prod-tf-course-edcox"
 }
 
 resource "aws_s3_bucket_acl" "prod_tf_course_acl" {
@@ -14,6 +14,20 @@ resource "aws_s3_bucket_acl" "prod_tf_course_acl" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "eu-west-1a"
+  tags = {
+    Terraform = "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "eu-west-1c"
+  tags = {
+    Terraform = "true"
+  }
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "Allow standard http and https ports inbound and everything outbound"
@@ -22,21 +36,21 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["163.116.162.118/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["163.116.162.118/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["163.116.162.118/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -47,7 +61,7 @@ resource "aws_security_group" "prod_web" {
 resource "aws_instance" "prod_web" {
   count = 2
 
-  ami           = "ami-0303d9db39b0722ac"
+  ami           = "ami-054ceb08a9e9f0607"
   instance_type = "t2.nano"
 
   vpc_security_group_ids = [
@@ -65,6 +79,33 @@ resource "aws_eip_association" "prod_web" {
 }
 
 resource "aws_eip" "prod_web" {
+  tags = {
+    Terraform = "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name = "prod-web"
+  #availability_zones = ["eu-west-1a", "eu-west-1c"]
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
   tags = {
     Terraform = "true"
   }
